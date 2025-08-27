6dc03894f3f0cf25b3650982e588f10dbd434443
Jonathan <ghuyers98@gmail.com>
2025-08-27 21:31:24 +0800

    set up layout

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   const t = await getTranslations("HomePage");
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">{t("title")}</h1>
         <Button size="sm">Button</Button>
--- apps/web/components/layout/header.tsx
+++ apps/web/components/layout/header.tsx
@@ -2,9 +2,11 @@ import { ThemeSwitcher } from "@/components/core/theme-switcher";
 
 export function Header() {
   return (
-    <header className="pointer-events-none sticky left-0 right-0 top-0 z-50 flex h-[48px] w-full items-center justify-between px-4 md:fixed md:top-28 md:px-8 lg:px-32">
-      <div className="pointer-events-auto">
-        <ThemeSwitcher />
+    <header className="bg-background pointer-events-none sticky left-0 right-0 top-0 z-50 flex h-[48px] w-full items-center justify-between md:fixed md:top-28 md:bg-transparent">
+      <div className="mx-auto grid w-full max-w-screen-2xl grid-cols-6 px-7 sm:px-8 md:grid-cols-12 md:px-10 lg:px-7">
+        <div className="pointer-events-auto col-start-1 col-end-7 flex w-full flex-row items-center justify-between gap-4 py-5 md:col-end-3 md:flex-col md:items-start md:py-0 lg:col-end-4 xl:col-start-2">
+          <ThemeSwitcher />
+        </div>
       </div>
     </header>
   );
