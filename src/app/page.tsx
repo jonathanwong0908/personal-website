cb58894ee4ed079d8ab12ab478b030edbe1e1bf9
Jonathan <jonathan@Jonathans-MacBook-Pro.local>
2025-12-21 13:54:55 +0800

    chore: setup mdx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-background flex min-h-screen w-full flex-col font-serif">
-      <div className="mx-auto flex w-full max-w-xl flex-col items-start gap-6 px-4 py-8 md:py-24">
+      <div className="mx-auto flex w-full max-w-xl flex-col items-start gap-6 px-4 py-8 md:py-32">
         <h1 className="text-2xl font-medium tracking-tighter">Jonathan Wong</h1>
         <div className="flex flex-col gap-1 text-sm">
           <p>🧑‍💻 Software Design and Development</p>
