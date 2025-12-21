08a932bd5bbc976675f55eae506624d52199d751
Jonathan <jonathan@Jonathans-MacBook-Pro.local>
2025-12-21 09:55:07 +0800

    chore: set up prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background"></div>
+    <div className="bg-background flex min-h-screen items-center justify-center">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-serif text-2xl tracking-tighter">Jonathan Wong</h1>
+        <p className="text-muted-foreground text-sm">Hong Kong</p>
+      </div>
+    </div>
   );
 }
