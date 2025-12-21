fbe091d890e834b928fc5771bfef4074ae63dd88
Jonathan <jonathan@Jonathans-MacBook-Pro.local>
2025-12-21 08:52:11 +0800

    init new project

--- apps/cms/app/tags/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TagsPage() {
-  return (
-    <div>
-      <div className="flex justify-between">
-        <h1 className="text-2xl font-bold">Tags</h1>
-      </div>
-    </div>
-  );
-}
--- apps/web/components/core/logo.tsx
+++
@@ -1,3 +0,0 @@
-export function Logo() {
-  return <span className="font-mono font-bold">J</span>;
-}
