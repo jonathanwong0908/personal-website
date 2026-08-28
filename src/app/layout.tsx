0fafa67f8d007524196b868a3cbb2e8c0856f007
Jonathan <ghuyers98@gmail.com>
2026-08-28 17:47:39 +0900

    feat: update design;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
+import type { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import localFont from "next/font/local";
 
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const coolvetica = localFont({
   src: "../../public/fonts/Coolvetica Rg.otf",
@@ -23,13 +20,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
-      <body
-        className={`${coolvetica.variable} font-sans antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${coolvetica.variable} font-sans text-lg antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,33 @@ export default function Home() {
       <div className="flex w-full flex-col items-start">
         <h1 className="text-lg leading-tight">Jonathan Wong</h1>
         <p className="text-muted-foreground text-lg">
-          I cook software, and food.
+          Software design and development
         </p>
       </div>
       <Separator />
       {/* <LatestThoughts /> */}
       <div className="flex h-full w-full gap-8 divide-x">
-        <aside className="flex flex-col gap-1.5 pr-8">
+        {/* <aside className="flex flex-col gap-1.5 pr-8">
           <p>About</p>
           <p className="text-muted-foreground">Thoughts</p>
-        </aside>
-        <main>
-          <p>Hello</p>
+        </aside> */}
+        <main className="flex flex-col gap-8">
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <h3 className="">Bio</h3>
+            </div>
+            <p className="text-muted-foreground">
+              Curious about life in general. Spending most of my time
+              continuously exploring technologies, places, people, experiences,
+              food, and quite a lot of other things.
+            </p>
+          </div>
+          {/* <div>
+            <h3>Thoughts</h3>
+          </div>
+          <div>
+            <h3>Working on</h3>
+          </div> */}
         </main>
       </div>
     </div>
